45bd34d7e5aff3bdfb4f0dfd9cbdc8465d943ca2
Lispooh13 <riaya1203@gmail.com>
2021-09-30 15:22:28 +0000

    [Add]アソシエーション

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,22 @@
 class Customer < ApplicationRecord
+  
+  has_many :orders, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :addresses, dependent: :destroy
+  
+  def full_name
+    first_name + " " + last_name
+  end
+  
+  def full_name_kana
+    first_name_kana + " " + last_name_kana
+  end
+  
+  # def has_in_cart(item)
+  #   cart_items.find_by(item_id: item.id)
+  # end
+
+
+
+
 end
